b322ab372df08c608b138adf71dcd2fa0ca315a4
mslipenchuk267 <mslipenchuk267@gmail.com>
2024-03-17 23:41:57 -0400

    Updates

---
+++ precache-manifest.1d5c98177a179c71d6b6c23ba724680b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6d9755c8e9dbd72cfa21ce44e9c1d2",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/my-site/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/my-site/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/my-site/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/my-site/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "68660066586a6fce60b1",
+    "url": "/my-site/static/js/runtime~main.f0333956.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-site/static/media/logo.06e73328.svg"
+  }
+]);
